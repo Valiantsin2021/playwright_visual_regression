55484c95117bfb2f18708dcafe140245f65ad212
Valiantsin Lutchanka <lutchenkovalentin@gmail.com>
2024-03-18 22:59:50 +0100

    initial commit

---
+++ fixtures/constants/constants.ts
@@ -0,0 +1,19 @@
+export const constants: {
+  baseURL: string
+  userName: string
+  failedLoginMsg: string
+  failedRegistrationMsg: string
+  subjectMsg: string
+  textMsg: string
+  filename: string
+  note: string
+} = {
+  baseURL: 'https://automationexercise.com/',
+  userName: 'Warehouse Manager',
+  failedLoginMsg: 'Your email or password is incorrect!',
+  failedRegistrationMsg: 'Email Address already exist!',
+  subjectMsg: 'Test subject',
+  textMsg: 'Test message to send via the Contuct Us form',
+  filename: './fixtures/test_data/valid/test.txt',
+  note: 'Below is the list of test Cases for you to practice the Automation. Click on the scenario for detailed Test Steps:',
+}
--- scripts/docker_build_and_run.sh
+++ scripts/docker_build_and_run.sh
@@ -2,6 +2,6 @@
 
 VERSION=$(cat ./package.json | grep '@playwright/test' | awk -F: '{ print $2 }' | sed 's/[",]//g' |tr  -d ^ | xargs)
 
-docker  build -t  playwright-test  -f /utils/docker/Dockerfile  --build-arg PW_VERSION="v$VERSION" .
+docker  build -t  playwright-test  -f ./utils/Docker/Dockerfile  --build-arg PW_VERSION="v$VERSION" .
 
 docker run --rm -it -v $(pwd):/app -w /app playwright-test npm test
