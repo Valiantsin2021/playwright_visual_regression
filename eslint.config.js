cd2a85d9241c5abdc8d19c6db2fcae317e188309
Valiantsin Lutchanka <lutchenkovalentin@gmail.com>
2024-05-22 21:31:27 +0200

    update linter

---
+++ eslint.config.js
@@ -0,0 +1,62 @@
+import prettier from 'eslint-config-prettier'
+import playwright from 'eslint-plugin-playwright'
+import globals from 'globals'
+import parser from '@typescript-eslint/parser'
+
+export default [
+  {
+    plugins: { prettier, playwright },
+    files: ['tests/*.spec.ts', 'fixtures/**/*.ts', 'pages/*.ts', 'utils/*.ts'],
+    languageOptions: {
+      parser,
+      parserOptions: {
+        sourceType: 'module',
+        project: ['tsconfig.json']
+      },
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+        __ENV: 'readonly'
+      }
+    },
+    rules: {
+      'playwright/no-conditional-in-test': 'off',
+      'playwright/expect-expect': 'off',
+      'playwright/no-focused-test': 'error',
+      'playwright/require-soft-assertions': 'warn',
+      'no-console': 'off',
+      'no-useless-escape': 'off',
+      'no-empty-pattern': 'off',
+      'no-eval': 'error',
+      'no-multi-spaces': 'error',
+      'no-new': 'warn',
+      'no-return-assign': 'warn',
+      'comma-dangle': ['error', 'never'],
+      strict: ['error', 'global'],
+      'func-style': ['warn', 'expression'],
+      'no-new-func': 'error',
+      'no-param-reassign': 'error',
+      'prefer-arrow-callback': 'error',
+      'arrow-body-style': ['error', 'as-needed'],
+      'no-invalid-this': 'error',
+      'prefer-destructuring': ['warn', { array: true, object: true }, { enforceForRenamedProperties: true }],
+      'no-implied-eval': 'error',
+      eqeqeq: 'error',
+      'no-with': 'error',
+      'func-call-spacing': ['error', 'never'],
+      'max-len': ['off', { code: 200, ignoreComments: true }],
+      'new-cap': ['error', { newIsCap: true }],
+      'new-parens': 'error',
+      quotes: ['warn', 'single', { avoidEscape: true, allowTemplateLiterals: true }],
+      'arrow-spacing': ['error', { before: true, after: true }],
+      'no-var': 'warn',
+      'no-unused-vars': ['warn', { vars: 'local' }],
+      'import/no-unresolved': 'off', // k6 is actually golang, can't really import it
+      'no-restricted-globals': 'off', // required by k6, e.g. "init" context
+      'import/extensions': 'off', // .js ending is ok
+      'no-prototype-builtins': 'off'
+    }
+  }
+]
--- scripts/docker_build_and_run.sh
+++ scripts/docker_build_and_run.sh
@@ -4,6 +4,4 @@ VERSION=$(cat ./package.json | grep '@playwright/test' | awk -F: '{ print $2 }' 
 
 docker  build -t  playwright-test  -f ./utils/Docker/Dockerfile  --build-arg PW_VERSION="v$VERSION" .
 
-npx playwright install
-
 docker run --rm -it -v $(pwd):/app -w /app playwright-test npm test
